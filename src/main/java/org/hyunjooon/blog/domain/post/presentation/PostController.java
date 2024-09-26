bea97ae9f2429bad3f6e1c437ac6e30be68db709
hyeonjoonpark <pjjoon1379@gmail.com>
2024-09-27 00:58:02 +0900

    FEAT(post) : Controller
    PostController를 생성하였습니다

---
+++ src/main/java/org/hyunjooon/blog/domain/post/presentation/PostController.java
@@ -0,0 +1,22 @@
+package org.hyunjooon.blog.domain.post.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/post")
+@RequiredArgsConstructor
+public class PostController {
+    // 게시물(post) 단일조회
+
+    // 게시물(post) 전체조회
+
+    // 게시물(post) 일부조회
+
+    // 게시물(post) 작성
+
+    // 게시물(post) 수정
+
+    // 게시물(post) 삭제
+}

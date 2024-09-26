9234856bed715770e335d9d60f182ec05a944d24
hyeonjoonpark <pjjoon1379@gmail.com>
2024-09-27 00:26:08 +0900

    FEAT(dto) : PostResponse
    DTO 파일을 작성하였습니다

---
+++ src/main/java/org/hyunjooon/blog/domain/post/presentation/dto/response/PostResponse.java
@@ -0,0 +1,29 @@
+package org.hyunjooon.blog.domain.post.presentation.dto.response;
+
+import jakarta.validation.constraints.Min;
+import lombok.Builder;
+import org.hyunjooon.blog.domain.comment.Comment;
+import org.hyunjooon.blog.domain.tag.Tag;
+import org.hyunjooon.blog.domain.user.User;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * DTO for {@link org.hyunjooon.blog.domain.post.Post}
+ */
+@Builder
+public record PostResponse(
+        LocalDateTime createdDate,
+        Long id,
+        String title,
+        String content,
+        User user,
+        List<Tag> tags,
+        List<Comment> comments,
+        @Min(0) int viewCount,
+        @Min(0) int likeCount
+) implements Serializable {
+
+}

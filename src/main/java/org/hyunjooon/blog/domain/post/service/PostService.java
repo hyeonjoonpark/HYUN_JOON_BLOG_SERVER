5fde2215047a1d0b8194acc998c574ba50f4c6c6
hyeonjoonpark <pjjoon1379@gmail.com>
2024-09-30 09:53:02 +0900

    FEAT(service) : PostService
    service 레이아웃 기초 틀을 잡았습니다

--- src/main/java/org/hyunjooon/blog/domain/post/service/PostService.java
+++ src/main/java/org/hyunjooon/blog/domain/post/service/PostService.java
@@ -1,8 +1,39 @@
 package org.hyunjooon.blog.domain.post.service;
 
+import lombok.RequiredArgsConstructor;
+import org.hyunjooon.blog.domain.post.presentation.dto.response.PostResponse;
+import org.hyunjooon.blog.domain.post.repository.PostRepository;
+import org.springframework.data.domain.Slice;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class PostService {
+    private final PostRepository postRepository;
+    @Transactional(readOnly = true, rollbackFor = Exception.class)
+    public PostResponse getPost() {
+        return null;
+    }
 
+    @Transactional(readOnly = true, rollbackFor = Exception.class)
+    public Slice<PostResponse> getPosts() {
+        return null;
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public ResponseEntity<?> createPost() {
+        return null;
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public ResponseEntity<?> updatePost() {
+        return null;
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public ResponseEntity<?> deletePost() {
+        return null;
+    }
 }

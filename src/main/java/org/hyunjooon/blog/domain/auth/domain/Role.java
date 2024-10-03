f057c66fbda2df834fd931567f96f2d175c545e6
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-03 14:15:10 +0900

    commit

--- src/main/java/org/hyunjooon/blog/domain/auth/domain/Role.java
+++ src/main/java/org/hyunjooon/blog/domain/auth/domain/Role.java
@@ -1,4 +1,36 @@
 package org.hyunjooon.blog.domain.auth.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hyunjooon.blog.domain.user.User;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> user = new HashSet<>();
+
+    @Builder
+    public Role(String name, Set<User> user) {
+        this.name = name;
+        this.user = user;
+    }
 }
